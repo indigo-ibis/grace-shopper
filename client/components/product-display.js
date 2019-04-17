54815c9bfca8980d4b18b58eed3ed6fa8a4a9200
Rhea Rao <imrhearao@gmail.com>
2019-04-17 16:33:22 -0400

    not sure what changed

---
+++ client/components/product-display.js
@@ -0,0 +1,52 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import {connect} from 'react-redux'
+import {loadSelectedProducts} from './../store'
+
+class productsDisplay extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            loaded : false
+        }
+    }
+
+    async componentDidMount() {
+        let houseName = productCategoryName = null;
+        if (this.props.match.params.house) {
+            houseName = this.props.match.params.house[0].toUpperCase() + this.props.match.params.house.slice(1).toLowerCase()
+        }
+        if (this.props.match.params.category) {
+            productCategoryName = this.props.match.params.category
+        }
+        await this.props.loadSelectedProducts()
+        .then( this.setState({
+            loaded : true
+        }) )
+    }
+
+    render() {
+        console.log(this.props.products)
+        if (!this.state.loaded) {return <h1>Loading...</h1>}
+        return (
+            <div>
+                {this.props.products.map(product => <h1 key={product.id}>{product.name}</h1>)}
+            </div>
+        )
+    }
+}
+
+const mapStateToProps = state => {
+    return {
+        products : state.products,
+        filters : state.filters
+    }
+}
+
+const mapDispatchToProps = dispatch => {
+    return {
+        loadSelectedProducts : (house, category) => dispatch(loadSelectedProducts(house, category))
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(productsDisplay)

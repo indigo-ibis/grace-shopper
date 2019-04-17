8adc4ef5d48e4212f24ad5d7fb0b853e570c1bd4
Marcia <mwan0906@gmail.com>
2019-04-17 17:04:10 -0400

    able to view product names on the front page filtered by house or by category through /products?house=house&category=category

--- client/routes.js
+++ client/routes.js
@@ -21,7 +21,7 @@ class Routes extends Component {
         {/* Routes placed here are available to all visitors */}
         <Route path="/login" component={Login} />
         <Route path="/signup" component={Signup} />
-        <Route path='/products/:house' component={ProductDisplay}/>
+        <Route path='/products' component={ProductDisplay}/>
         {isLoggedIn && (
           <Switch>
             {/* Routes placed here are only available after logging in */}

1b3ce8bc241ae76f93503407a00bd930730276c1
Marcia <mwan0906@gmail.com>
2019-04-18 11:52:36 -0400

    adjusted product display css

--- client/components/single-product-display.js
+++ client/components/single-product-display.js
@@ -3,8 +3,9 @@ import {Link} from 'react-router-dom'
 
 export default props => {
   return (
-    <div>
-      <h1><Link to={`/products/${props.id}`}>{props.name}</Link></h1>
+    <div className='singleProductCell'>
+      <img className='preview' src={props.imageUrl}></img>
+      <h3><Link to={`/products/${props.id}`}>{props.name}</Link></h3>
       Price: {(props.price / 100).toLocaleString("en-US", {style:"currency", currency:"USD"})}
     </div>
   )

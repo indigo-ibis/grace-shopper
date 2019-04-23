6ace126d2bc5f64323617f99d44e7c0a4e086050
Daniel L <daniellanoff@gmail.com>
2019-04-23 13:33:29 -0400

    more admin functionality

--- server/api/admin.js
+++ server/api/admin.js
@@ -1,8 +1,10 @@
 const router = require('express').Router()
 const {Order, LineItem, User} = require('../db/models')
+const {adminGateway} = require('./gateways')
+
 module.exports = router
 
-router.get('/allOrders', async (req, res, next) => {
+router.get('/allOrders', adminGateway, async (req, res, next) => {
   try {
     const orders = await Order.findAll()
     res.json(orders)
@@ -12,7 +14,7 @@ router.get('/allOrders', async (req, res, next) => {
   }
 })
 
-router.get('/allUsers', async (req, res, next) => {
+router.get('/allUsers', adminGateway, async (req, res, next) => {
   try {
     const user = await User.findAll()
     res.json(user)
@@ -22,7 +24,7 @@ router.get('/allUsers', async (req, res, next) => {
 })
 
 //Find all items
-router.get('/allitems', async (req, res, next) => {
+router.get('/allitems', adminGateway, async (req, res, next) => {
   try {
     const lineItems = await LineItem.findAll()
     console.log(lineItems)
@@ -33,7 +35,7 @@ router.get('/allitems', async (req, res, next) => {
 })
 
 // creating a new order (collection of order items)
-router.post('/', async (req, res, next) => {
+router.post('/', adminGateway, async (req, res, next) => {
   try {
     const newOrder = await Order.create({
       // checks if there was a userId sent (meaning they're logged in), otherwise null
@@ -47,23 +49,37 @@ router.post('/', async (req, res, next) => {
 })
 
 // editing an order's fullfillment Status
-// router.put('/:newStatus', async (req, res, next) => {
-//   try {
-//     const order = await Order.findByPk(req.params.orderId)
-//     if (!order) {
-//       res.sendStatus(404)
-//     } else {
-//       await order
-//         .update({fullfillmentStatus: req.body.fullfillmentStatus})
-//         .then(res.json(order))
-//     }
-//   } catch (err) {
-//     next(err)
-//   }
-// })
+router.put('/changeorderstatus/:orderId/:newStatus', async (req, res, next) => {
+  try {
+    const order = await Order.findByPk(req.params.orderId)
+    if (!order) {
+      res.sendStatus(404)
+    } else {
+      await order
+        .update({fullfillmentStatus: req.params.newStatus})
+        .then(res.json('success'))
+    }
+  } catch (err) {
+    next(err)
+  }
+})
+
+router.put('/checkout', async (req, res, next) => {
+  try {
+    await Order.update(
+      {
+        fullfillmentStatus: 'Unfullfilled'
+      },
+      {where: {userId: req.user.id}}
+    )
+    res.sendStatus(201)
+  } catch (err) {
+    next(err)
+  }
+})
 
 // clear an entire order/cart
-router.get('/:orderId/clear', async (req, res, next) => {
+router.get('/:orderId/clear', adminGateway, async (req, res, next) => {
   try {
     await LineItem.destroy({
       where: {
